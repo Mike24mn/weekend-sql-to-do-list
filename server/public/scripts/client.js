6f68a454bc7d888f0648afb19fb5999fbcca10f3
Michael LaFond <Mike24mn@gmail.com>
2024-05-17 20:30:09 -0500

    created logic for client.js and todos.router.js, then connected routes.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,166 @@
 console.log('JS is sourced!');
+
+console.log("js");
+
+function getToDos() {
+  console.log("in getToDos");
+  // axios call to server to get to dos
+  axios.get("/todos").then((response) => {
+    renderToDos(response.data); // render to dos
+  });
+} // end getToDos
+
+function deleteToDos(todosId) {
+  axios({
+    method: "DELETE",
+    url: `/todos/${todosId}`,
+  })
+    .then((response) => {
+      console.log("todo targetted for delete: ", todosId);
+      refreshToDos(); // refresh to dos
+    })
+    .catch((error) => {
+      alert("Critical error, check deleteToDos");
+    });
+}
+
+function saveToDos(todoToAdd) {
+  console.log("in saveToDos");
+  // axios call to server to get to dos
+
+  axios({
+    method: "POST",
+    url: "/todos",
+    data: todoToAdd,
+  })
+    .then(function (response) {
+      console.log("saveToDo()", response.data);
+      refreshToDos();
+      //needs to be refresh not render as the newly added quoala needs to be updated by get in database
+    })
+    .catch(function (error) {
+      console.log("Error in POST", error);
+      alert("Unable to add to do at this time. Please try again later.");
+    });
+}
+
+//add a RenderToDos
+
+function renderToDos(todo) {
+  let todoTable = document.getElementById("viewToDos");
+  //might have to let if program trouble
+  todoTable.innerHTML = "";
+
+  for (let i = 0; i < todo.length; i += 1) {
+    let currentToDo = todo[i];
+    // For each to do, append a new row to our table
+    if (todo.isComplete === false) {
+      todoTable.innerHTML += `
+      <tr>
+        <td>${todo.text}</td>      
+        <td>${todo.isComplete}</td>
+       <td> 
+       <button onClick= "isFinished(${todo.isComplete}, ${todo.id})">
+    Goal Complete
+    </button>
+    </td>
+    <td>
+    <button onClick="deleteToDos(${todo.id})">
+    Delete
+    </button>
+    </td>
+      </tr>
+
+
+    `;
+    } else if (todo.isComplete === true) {
+      todoTable.innerHTML += `
+      <tr>
+        <td>${todo.text}</td>      
+       <td>${todo.isComplete}</td>
+       <td>
+       <button onClick="deleteToDos(${todo.id})">
+       Delete
+       </button>
+       </td>
+
+       <td> 
+      </td>
+      </tr>
+
+
+    `;
+    }
+  }
+}
+
+//add refresh To Dos
+function refreshToDos() {
+  axios({
+    method: "GET",
+    url: "/todos",
+  })
+    .then(function (response) {
+      console.log("refreshToDos() response", response.data);
+      renderToDos(response.data);
+    })
+    .catch(function (error) {
+      console.log("error in GET", error);
+    });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let todo = {};
+
+  todo.text = document.getElementById("toDoItem").value;
+
+  saveToDos(todo);
+  let form = document.getElementById("form");
+  form.reset();
+}
+//get the info from the input
+
+//call saveToDos
+
+//clear the form
+
+//getToDos();
+
+// Fuction to change boolean value of complete task, AKA displys whether a to do is completed or not
+// based on booleans true/false
+
+// May need to change to do parameter below
+// Unfinished, left off here - Michael
+
+function isFinished(complete, todoId) {
+  console.log("changing transfer status: ", complete, todoId);
+
+  // use axios to send a PUT request to change song rank
+  // Send direction and id in URL
+  // For .then, will call the render function to change the DOM
+
+  // note to self, removed isRead from url
+
+  axios({
+    method: "PUT",
+    url: "/todos/" + todoId,
+    data: {
+      complete: complete,
+    },
+  })
+    .then((response) => {
+      refreshToDos();
+      // refreshToDos() will retrieve all to dos and then update the DOM
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+}
+
+
+/* todos has
+// text and isComplete, also id 
+*/
+
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,94 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+//DONT forget the require 
+//note that pool goes up by ..
+// DB CONNECTION
 
+
+// GET
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos";';
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    }) 
+      .catch(error => {
+        console.log('error getting to do info', error);
+        res.sendStatus(500);
+      });
+  });
+
+// POST
+router.post('/', (req, res) => {
+    let newToDo = req.body;
+    console.log(`Adding To Do`, newToDo);
+  
+
+    let queryText = `INSERT INTO "todos" ("text", "isComplete")
+                     VALUES ($1, $2);`;
+    pool.query(queryText, [newToDo.text, newToDo.isComplete])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new to do, line 35`, error);
+        res.sendStatus(500);
+      });
+  });
+
+// PUT
+router.put('/:id', (req, res) => {
+
+    
+    let todoId = req.params.id;
+    //not sure if I should change direction 
+    //isRead refers to the body as the isRead is in the data part of put request
+    let complete  = req.body.isComplete;
+  
+    let queryText = `
+    UPDATE "todos" SET "isComplete"= NOT "isComplete"
+    WHERE "id"= $1;
+`
+    console.log("Change complete: ", todoId, isComplete)
+  
+  //line 56 confuses me 
+  
+  
+    //in the below statement [] turns the todoId into an Array
+
+    // pool.query needed to take queryText and both todoId and transfer, it was missing transfer - Michael
+
+    pool.query(queryText, [todoId])
+      .then((result) => {
+        res.sendStatus(204)
+      })
+      .catch((err) => {
+        console.log(`Error making query.. '${queryText}'`, err)
+        res.sendStatus(500)
+      })
+  })
+
+// DELETE
+
+router.delete('/:id', (req, res) => {
+
+  let reqId = [req.params.id]
+
+  let queryText = `DELETE FROM "todos" WHERE "id" = $1;`
+
+  pool.query(queryText, reqId)
+  .then((result) => {
+      console.log("delete success");
+      res.sendStatus(200)
+  })
+  .catch((err) => {
+      console.log("error making query...", `${queryText}`, err);
+      res.sendStatus(500)
+
+  })
+ 
+});
 
 
 module.exports = router;
